74e3412af99900ff9f6d30ec81a8adbe6695c6f2
Masayuki <soy2.san@gmail.com>
2021-02-27 23:15:39 +0900

    Initial Commit

---
+++ MySteps/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MySteps
+//
+//  Created by 山口雅之 on 2021/02/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MySteps/MyStepsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyStepsApp.swift
+//  MySteps
+//
+//  Created by 山口雅之 on 2021/02/27.
+//
+
+import SwiftUI
+
+@main
+struct MyStepsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
